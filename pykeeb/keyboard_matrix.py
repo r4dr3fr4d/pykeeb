671d77dbdabdd7f2f3684b57c9b77efb9bdabf69
raycewest <rwest@g.clemson.edu>
2018-01-28 21:12:15 -0500

    mx_notches to init()

--- pykeeb/keyboard_matrix.py
+++ pykeeb/keyboard_matrix.py
@@ -4,7 +4,7 @@ from .keyswitch_mount import *
 from math import asin,sin,cos,acos,degrees,radians
 
 class Keyboard_matrix:
-	def __init__(self, r, c, row_spacing=1.5, column_spacing=1.5, plate_thickness=3, origin=[0,0,0], x_tent=0, y_tent=0, z_tent=0, mount_length=DSA_KEY_WIDTH, mount_width=DSA_KEY_WIDTH):
+	def __init__(self, r, c, row_spacing=1.5, column_spacing=1.5, plate_thickness=3, origin=[0,0,0], x_tent=0, y_tent=0, z_tent=0, mount_length=DSA_KEY_WIDTH, mount_width=DSA_KEY_WIDTH, switch_type='alps', mx_notches=True):
 		self.rows = r
 		self.columns = c
 		self.modifiers = modifiers
@@ -29,6 +29,8 @@ class Keyboard_matrix:
 		self.side_extrude = 3
 		self.wall_x = .01 #for extra wall_hull thickness
 		self.wall_y = .01 #for extra wall_hull thickness
+		self.switch_type = switch_type
+		self.mx_notches = mx_notches
 
 		self.rm = self.row_modifiers = [[0, 0, 0, 0, 0, 0]  for b in range(r)] 
 		self.cm = self.column_modifiers = [[0, 0, 0, 0, 0, 0]  for a in range(c)] 
@@ -37,7 +39,7 @@ class Keyboard_matrix:
 		self.generate()
 
 
-	def project_rows(self, R):
+	def arc_rows(self, R):
 
 		unit_width=(self.row_spacing+self.mount_width)
 		unitangle=degrees(2*asin(unit_width/(2*R)))
@@ -56,7 +58,7 @@ class Keyboard_matrix:
 		    xt=(focus_x+(sin(radians(theta))*(R+7)))-x
 		    self.rm[row]= [0, xt, zt, theta, 0, 0]
 
-	def project_cols(self, R):
+	def arc_cols(self, R):
 
 		unit_width=(self.column_spacing+self.mount_width)
 		unitangle=degrees(2*asin(unit_width/(2*R)))
@@ -84,7 +86,8 @@ class Keyboard_matrix:
 		modifiers = [[list(map(sum, zip(self.rm[row], self.cm[column], self.im[row][column]))) for column in range(self.columns)] for row in range(self.rows)]
 		modifiers = [[modifiers[row][column] + [self.ik[row][column]] for column in range(self.columns)] for row in range(self.rows)]
 
-		self.sm = self.switch_matrix = [[Keyswitch_mount([list(map(sum, zip(modifiers[row][column][:3], [column * (self.mount_width + self.column_spacing), row * (self.mount_length + self.row_spacing), 0]))) + modifiers[row][column][3:6], [self.origin[0], self.origin[1], self.origin[2], self.x_tent, self.y_tent, self.z_tent]], modifiers[row][column][6]) for column in range(self.columns)] for row in range(self.rows)]
+		#def __init__(self, transformations, ik=False, switch_type='alps', mount_length=DSA_KEY_WIDTH, mount_width=DSA_KEY_WIDTH, mx_notches=True):
+		self.sm = self.switch_matrix = [[Keyswitch_mount([list(map(sum, zip(modifiers[row][column][:3], [column * (self.mount_width + self.column_spacing), row * (self.mount_length + self.row_spacing), 0]))) + modifiers[row][column][3:6], [self.origin[0], self.origin[1], self.origin[2], self.x_tent, self.y_tent, self.z_tent]], modifiers[row][column][6], self.switch_type, self.mount_length, self.mount_width, self.mx_notches) for column in range(self.columns)] for row in range(self.rows)]
 
 		self.row_hulls = [[(self.sm[row][column].get_front(self.row_hull_thickness, self.row_hull_extrude) + self.sm[row+1][column].get_back(self.row_hull_thickness, self.row_hull_extrude)).hull() for column in range(self.columns)] for row in range(self.rows-1)] 
 
